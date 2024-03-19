291e63bd628b7ff52e97f9de704a9e8c2338ad35
Tan Chia Qian <42717486+honhaochen@users.noreply.github.com>
2024-03-19 08:29:48 +0800

    revise story's image

--- src/app/_components/story.tsx
+++ src/app/_components/story.tsx
@@ -18,23 +18,28 @@ const Story = () => {
     count === 1 ? "/assets/girl_smile.png" : "/assets/girl.png";
 
   return (
-    <div className="h-[90vh] flex items-center bg-rose-950 justify-center rounded-3xl border-4 border-slate-200 my-10">
-      <img
-        className={`w-[20vw] h-[30vh] ${styles.left}`}
-        style={{
-          backgroundImage: `url(${backgroundGirlImageUrl})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      />
-      <img
-        className={`w-[20vw] h-[30vh] ${styles.right}`}
-        style={{
-          backgroundImage: `url(${backgroundBoyImageUrl})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      />
+    <div className="h-[90vh] flex flex-col items-center bg-rose-950 justify-center rounded-3xl border-4 border-slate-200 my-10">
+      <div className="text-2xl mt-4 font-bold font-sans text-white">
+        执子之手，与子偕老
+      </div>
+      <div className="flex justify-center items-center">
+        <div
+          className={`w-[20vw] h-[30vh] ${styles.left}`}
+          style={{
+            backgroundImage: `url(${backgroundGirlImageUrl})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <div
+          className={`w-[20vw] h-[30vh] ${styles.right}`}
+          style={{
+            backgroundImage: `url(${backgroundBoyImageUrl})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+      </div>
     </div>
   );
 };
