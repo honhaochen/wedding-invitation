2ce07df32dd2eca0acc7d005e315fbab3fdbf9e3
honhaochen <42717486+honhaochen@users.noreply.github.com>
2024-04-24 23:32:10 +0800

    revert to portrait only for location

--- src/app/_components/location.tsx
+++ src/app/_components/location.tsx
@@ -1,12 +1,10 @@
 "use client";
 
-import { LatLngExpression } from "leaflet";
 import styles from "@/app/_styles/Common.module.css";
 import { useContext, useState, useEffect } from "react";
 import { PageContext } from "./container";
 
 const Location = () => {
-  const position: LatLngExpression = [3.1266427658925164, 101.72525945920253];
   const activeIndex = useContext(PageContext);
   const [show, setShow] = useState(false);
   useEffect(() => {
@@ -16,12 +14,12 @@ const Location = () => {
   }, [activeIndex]);
 
   return (
-    <div className="h-[90vh] flex flex-col items-center justify-center rounded-3xl border-4 border-white my-10 md:flex-row md:p-12">
+    <div className="h-[90vh] flex flex-col items-center justify-center rounded-3xl border-4 border-white my-10">
       {show && (
         <div
-          className={`flex flex-col items-center justify-center ${styles.fadeOut} md:flex-row md:p-12`}
+          className={`flex flex-col items-center justify-center ${styles.fadeOut}`}
         >
-          <div className="w-[80vw] p-8 md:w-3/5 flex items-center justify-center flex-col md:items-start">
+          <div className="w-[80vw] p-8 flex items-center justify-center flex-col">
             <div className="text-4xl font-bold mt-4 font-chinese text-off-white-dark">
               婚礼地点
             </div>
@@ -31,12 +29,12 @@ const Location = () => {
             <div className="text-lg font-chinese mt-2 text-off-white-dark">
               喜粤饮食集团
             </div>
-            <div className="text-lg font-display mt-1 text-off-white-dark text-center md:text-left">
+            <div className="text-lg font-display mt-1 text-off-white-dark text-center">
               Sunway Velocity Grand Imperial Group
             </div>
           </div>
           <div
-            className="w-[70vw] h-[40vh] md:w-2/5"
+            className="w-[70vw] h-[40vh]"
             style={{
               backgroundImage: `url('/assets/map.png')`,
               backgroundSize: "contain",
